901da4e8abac9ef5381371c456836ea058d53daf
toru <toru2.ogura@gmail.com>
2018-06-27 11:36:28 +0900

    vcs: radDirac.Form1.csをcommit

--- SymplecticIntegral/Star/Star/Form1.cs
+++ SymplecticIntegral/Star/Star/Form1.cs
@@ -17,7 +17,7 @@ namespace Star
         const double Mass = 1.0; // mass of a star
         const double G = 1.0; // Gravity constant
         //---- experimental settings
-        const double dT = 1.0 / 32.0; // time slice
+        const double dT = 1.0 / 100.0; //1.0/32.0 この設定では2つの星が近づき過ぎると軌道が変になる; // time slice
         const int N = 4;          // number of stars
 
         //---- declaration of structure Matter
@@ -28,7 +28,12 @@ namespace Star
             public Vector2 f; //force
         }
 
-        string[,] data; //animation用
+        //string[,] data; //animation用
+        Matter[] M = new Matter[N];
+        int x = 0;  // 球の x 座標
+        int y = 0;  // 球の y 座標
+        int[] xp = new int[N];
+        int[] yp = new int[N];
 
         public Form1()
         {
@@ -116,43 +121,33 @@ namespace Star
             return E;
         }
 
-        double[] x, y0, y1, y;
 
-        void drawWave(Graphics g)
+        void draw(Graphics g)
         {
-            int i;
-            float xc = 50;
-            double mp = 4.0;
-
+            float xc = 400;
             g.TranslateTransform(xc, this.Height / 2);//座標軸の移動
             g.ScaleTransform(1.0f, -1.0f);//座標軸の反転, float指定が必要
-
-            Pen myPen = new Pen(Color.Blue, 1);
-            int m = x.Length;
-            for (i = 0; i < m - 1; i++)
+            for (int j = 0; j < N; j++)
             {
-                g.DrawLine(myPen, (float)x[i], (float)(y1[i] * mp), (float)x[i + 1], (float)(y1[i + 1] * mp));
+                Rectangle rect = new Rectangle(xp[j], yp[j], 4, 4);
+                g.FillEllipse(new SolidBrush(Color.Blue), rect);
+                g.DrawEllipse(new Pen(Color.Blue), rect);
             }
-            myPen.Dispose();
-            //g.Dispose();これがあるとエラー
+
         }
 
-
-        void calc_wave()
-        {
-            int i;
-            int m = y0.Length;
-            for (i = 1; i < m - 1; i++) y[i] = y1[i + 1] + y1[i - 1] - y0[i];
-            for (i = 1; i < m - 1; i++)
-            {
-                y0[i] = y1[i];
-                y1[i] = y[i];
-            }
-        }
 
         void timer_Tick(object sender, EventArgs e)
         {
-            calc_wave();
+            Evolve(M, dT);
+            double mx = 50;
+            double my = 50;
+            for (int i = 0; i < N; i++)
+            {
+                xp[i] = (int)((M[i].q).x * mx);
+                yp[i] = (int)((M[i].q).y * my);
+            }
+
             this.Invalidate();  // 再描画を促し,OnPaint起動
         }
 
@@ -161,52 +156,78 @@ namespace Star
             base.OnPaint(e);
 
             Graphics g = e.Graphics;
-            drawWave(g);
+
+            draw(g);
+            //Evolve(M, dT); //これがあるとNG
             //System.Threading.Thread.Sleep(100);//タイマーを使用する場合は不要かも
         }
-
-        //private void Form1_Load(object sender, EventArgs e)
-        //{
-        //    //2012 Jun 20, ダブルバッファリング
-        //    //2011 Aug 27, 一次元双曲型、波動、差分法
-        //    int i, m;
-        //    double a, L, dx;
-        //    a = 50.0; L = 500.0;
-        //    m = 1000;
-        //    y0 = new double[m + 1];
-        //    y1 = new double[m + 1];
-        //    y = new double[m + 1];
-        //    x = new double[m + 1];
-
-        //    dx = L / m;
-        //    for (i = 0; i <= m; i++) x[i] = dx * (double)i;
-
-        //    //for (i = 0; i <= 6; i++) y0[i] = a * Math.Sin(Math.PI * (double)i / 6.0);
-        //    for (i = 0; i <= 50; i++) y0[i] = a * Math.Sin(Math.PI * (double)i / 50.0);
-        //    y1[0] = 0.0; y1[m] = 0.0;
-        //    for (i = 1; i < m; i++) y1[i] = (y0[i + 1] + y0[i - 1]) / 2.0;
-
-
-        //    Timer timer;
-        //    timer = new Timer()
-        //    {
-        //        Interval = 5,
-        //        Enabled = true,//timer.Start()と同じ
-        //    };
-
-        //    timer.Tick += new EventHandler(timer_Tick);
-
-        //    this.DoubleBuffered = true;  // ダブルバッファリング
-        //    //this.BackColor = SystemColors.Window;
-
-        //}
-
 
 
 
         private void button1_Click(object sender, EventArgs e)
         {
             //star
+            //useChart use = new useChart();
+            //use.chartSetup("Title", "Axis X", "Axis Y", "series-1");
+            //double xMin = -5.0;
+            //double xMax = 5.0;
+            //double yMin = -5.0;
+            //double yMax = 5.0;
+            //use.xyLimit(xMin, xMax, yMin, yMax);
+
+
+            double T = 0.0;
+            double Te = 50.0; //20.0
+            int ct = (int)(Te / dT);
+
+            //string[,] data = new string[N, ct];
+            Console.WriteLine("ct= {0:d}", ct);
+
+            Init(M);
+
+            //運動をanimation描画 data[j, i]をglobalにする
+            Timer timer;
+            timer = new Timer()
+            {
+                Interval = 1, //5
+                Enabled = true,//timer.Start()と同じ
+            };
+            timer.Tick += new EventHandler(timer_Tick);
+            this.DoubleBuffered = true;  // ダブルバッファリング
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //star timer不使用
+            // "アプリケーションはブレークモードになっています" のメッセージが出て止まるので
+            // ツール-->オプション-->デバッグ-->全般　"マネージ互換モードの使用"にをチェックを入れる
+            Init(M);
+
+            this.DoubleBuffered = true;
+
+
+            for (int k = 0; k < 50000; k++)
+            {
+                //Evolve(M, dT);
+                double mx = 50;
+                double my = 50;
+                for (int i = 0; i < N; i++)
+                {
+                    xp[i] = (int)((M[i].q).x * mx);
+                    yp[i] = (int)((M[i].q).y * my);
+                }
+
+                this.Refresh();//OnPaingを起動
+                System.Threading.Thread.Sleep(1);
+                Evolve(M, dT);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // chartControlで表示
             useChart use = new useChart();
             use.chartSetup("Title", "Axis X", "Axis Y", "series-1");
             double xMin = -5.0;
@@ -215,20 +236,18 @@ namespace Star
             double yMax = 5.0;
             use.xyLimit(xMin, xMax, yMin, yMax);
 
-
             double T = 0.0;
-            Matter[] M = new Matter[N];
-            double Te = 50.0; //20.0
+            double Te = 110.0; //20.0
             int ct = (int)(Te / dT);
 
-            //string[,] data = new string[N, ct];
-            data = new string[N, ct];
-            Console.WriteLine("ct= {0:%d}", ct);
+            string[,] data = new string[N, ct];
+            Console.WriteLine("ct= {0:d}", ct);
 
             Init(M);
-	        for (int i = 0; i < ct; i++)
+
+            for (int i = 0; i < ct; i++)
             {
-		        for (int j = 0; j<N; j++)
+                for (int j = 0; j < N; j++)
                 {
                     data[j, i] = ((M[j].q).x).ToString() + " " + (M[j].q.y).ToString();
                     use.setScatter((M[j].q).x, (M[j].q).y);
@@ -237,25 +256,10 @@ namespace Star
                 Evolve(M, dT);
 
                 T += dT;
-		        // fmt.Println(*M[0].q)
-	        }
+                // fmt.Println(*M[0].q)
+            }
             use.showChart();
 
-            //運動をanimation描画 data[j, i]をglobalにする
-            
-
-
-            //string fname := "C:/Users/OGURA/go/practice/ComputationPhysics/symplecticIntegration/data01.txt"
-            //FwriteLines(fname, data[0])
-
-            //   fname = "C:/Users/OGURA/go/practice/ComputationPhysics/symplecticIntegration/data02.txt"
-            //FwriteLines(fname, data[1])
-
-            //   fname = "C:/Users/OGURA/go/practice/ComputationPhysics/symplecticIntegration/data03.txt"
-            //FwriteLines(fname, data[2])
-
-            //   fname = "C:/Users/OGURA/go/practice/ComputationPhysics/symplecticIntegration/data04.txt"
-            //FwriteLines(fname, data[3])
         }
     }
 }
---
+++ practice/radDirac/radDirac/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace radDirac
+{
+
+    public interface IOrbital
+    {
+        void Assign(int _n, int _l, double _j);
+        void Adjust();
+        //void Draw(void);
+        void Output();
+        double CalcDG(int i, double[] P, double G, double F);
+        double CalcDF(int i, double[] P, double G, double F);
+
+        int PreInteg(double[] P);
+        int HammingA(double[] P, int i_jnt);
+        int HammingB(double[] P, int i_jnt);
+        void Normalize(double a, int i_jnt);
+
+    }
+
+    public partial class Form1 : Form
+    {
+        //=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+        //    Program raddirac
+        //=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=
+
+
+
+        //---- experimental settings
+        const double Z = 1.0;
+        public const double DX = 0.0625;        // size of a cell in x-space
+        public const int R_NUM = 256;           // total number of grids
+        public const int R_FAR = R_NUM - 1;       // index of farthest grid
+        const int R_O = 160;           // index of Bohr radius
+
+        //---- graphics settings
+        //const int WIN_WIDTH = 512;
+        //const int WIN_HEIGHT = 256;
+        //const int BASE_Y = 160;
+        //const double MAG_Y = 196.0;
+        //const double OUTPUT_R_MAX = 64.0;       // max radius output [a.u]
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //---- table of Radius
+        public static double[] R;
+
+        //---- declaration of class Potential
+        public static double[] U; // rV, potential muliplied by radius
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //radDirac
+            //Orbital O = new Orbital();
+
+            R = new double[R_NUM];
+            for (int i = 0; i <= R_FAR; i++)
+            {
+                R[i] = (1.0 / Z) * Math.Exp(DX * (i - R_O));
+            }
+
+            U = new double[R_NUM];
+            for (int i = 0; i <= R_FAR; i++)
+            {
+                U[i] = -Z;                      // Coulomb potential of nuclear
+            }
+
+            Orbital O = new Orbital(); //R, Uの設定の前に置くとR, UがNullでエラー
+            for (int n = 1; n <= 3; n++)
+            {
+                for (int l = 0; l <= n - 1; l++)
+                {
+                    for (double j = l - 0.5; j <= l + 0.5; j += 1.0)
+                    {
+                        if (j < 0.0) continue;
+
+                        O.Assign(n, l, j);
+                        O.Adjust();
+                        //O.Output();
+                    }
+                }
+            }
+
+            Console.WriteLine("End");
+        }
+
+
+        ////---- draws wave function
+        ////void Orbital::Draw(void)
+        ////{
+        ////	const double MAG_X = (double)WIN_WIDTH / OUTPUT_R_MAX;
+        ////	NXDrawMoveto();
+        ////	for (int i = 0; i <= R_FAR; i++) {
+        ////		NXDrawLineto(int(R[i] * MAG_X), BASE_Y - int(G[i] * MAG_Y));
+        ////	}
+        ////}
+
+
+
+    }
+}
